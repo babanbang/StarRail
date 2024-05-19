f151bfdd194e6124be3bdb6a362dc7a1ca0fad6b
bbaban <zasd32345@gmail.com>
2024-05-19 18:30:47 +0800

    细节优化

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { getDirPath } from '#Mys.tool'
+import { Cfg, getDir } from '#Mys.tool'
+
+const dir = getDir(import.meta.url)
+for (const type of ['artifact', 'character', 'weapon']) {
+  await import(`file://${dir.path}/resources/meta/${type}/index.js`)
+}
+Cfg.initCfg('/components/' + dir.name, 'sr')
+
 export * from './Apps/role.js'
 export * from './Apps/profile.js'
 export * from './Apps/explore.js'
-
-const Path = getDirPath(import.meta.url)
-for (const type of ['artifact', 'character', 'weapon']) {
-  await import(`file://${Path}/resources/meta/${type}/index.js`)
-}
