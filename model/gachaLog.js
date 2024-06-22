98b1829b8b49f7b9e8b84a7c67c97393ba104987
bbaban <zasd32345@gmail.com>
2024-06-23 01:03:22 +0800

    导出记录

--- model/gachaLog.js
+++ model/gachaLog.js
@@ -141,7 +141,6 @@ export default class Role extends Base {
     const tmpId = {}
     pool.forEach(({ type, typeName }) => {
       const gacha = this.readJson(type).list.reverse()
-      res[type] = gacha
       gacha.forEach(v => {
         let id = v.id
         if (!id) {
@@ -183,7 +182,15 @@ export default class Role extends Base {
     const savePath = Data.writeJSON(save, uigfData, { root: true })
 
     logger.mark(`${this.e.logFnc} 导出成功${this.uid}.json`)
-    this.e.reply(`导出成功：${this.uid}.json，共${list.length}条 \n请接收文件`)
+    this.e.reply(`导出成功：${this.uid}.json，共${list.length}条`)
+
+    try {
+      if (this.e.isGroup) {
+        await this.e.bot.UploadGroupFile(this.e.group_id, savePath, `${this.uid}.json`)
+      } else {
+        await this.e.bot.UploadPrivateFile(this.e.user_id, savePath, `${this.uid}.json`)
+      }
+    } catch (err) { }
 
     Data.delFile(save, { root: true })
   }
