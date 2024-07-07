cc27904c3230d181e2e22f65eabead3dbdf32bee
bbaban <zasd32345@gmail.com>
2024-07-07 19:27:55 +0800

    调整命令

--- Apps/role.js
+++ Apps/role.js
@@ -4,7 +4,7 @@ import Role from '../model/role.js'
 
 const reg = MysUtil.reg.sr
 export const role = karin.command(
-  new RegExp(`^${reg}?(角色|查询|查询角色|角色查询)((18|[1-9])[0-9]{8})*$|^${reg}?uid(\\+|\\s)*(18|[1-9])[0-9]{8}$|^#(18|[1-9])[0-9]{8}`, 'i'),
+  new RegExp(`^${reg}?(角色|查询角色|角色查询)((18|[1-9])[0-9]{8})*$`, 'i'),
   async (e) => {
     const img = await new Role(e).roleList()
     if (img) e.reply(img)
